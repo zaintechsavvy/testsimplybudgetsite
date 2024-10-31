c7327fbd58ff3320fac3481b4e72d9425526944b
myzainpirani <myzainpirani@gmail.com>
2024-10-31 22:44:31 +0000

    bankimage

--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -12,26 +12,34 @@ const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => (
     } flex items-center justify-center min-w-[170px] h-[700px] transition-[flex] duration-[0.7s] ease-out-flex cursor-pointer`}
     onClick={() => handleClick(id)}
   >
-    <img src={imgUrl} alt={title} className="absolute w-full h-full object-cover rounded-[24px]" />
+    <img
+      src={imgUrl}
+      alt={title}
+      className="absolute w-full h-full object-cover lg:object-contain rounded-[24px]"
+    />
 
-    { active !== id ? (
+    {active !== id ? (
       <h3 className="font-semibold sm:text-[26px] text-[18px] text-white absolute z-0 lg:bottom-20 lg:rotate-[-90deg] lg:origin-[0,0]">
         {title}
       </h3>
     ) : (
       <div className="absolute bottom-0 p-8 justify-start w-full flex-col bg-[rgba(0,0,0,0.5)] rounded-b-[24px]">
         <div className={`${styles.flexCenter} w-[60px] h-[60px] glassmorphism mb-[16px] rounded-[12px]`}>
-          <img src="/headset.svg" alt="headset" className="w-1/2 h-1/2 object-cotain" />
+          <img
+            src="/headset.svg"
+            alt="headset"
+            className="w-1/2 h-1/2 object-cotain"
+          />
         </div>
-{/* Suggested code may be subject to a license. Learn more: ~LicenseLog:1169339816. */}
-        <p className="font-normal text-[16px] leading-[20.16px] text-white mb-[16px]">We are actively working on intigration with Plaid to connect your accounts</p>
+        <p className="font-normal text-[16px] leading-[20.16px] text-white mb-[16px]">
+          We are actively working on integration with Plaid to connect your accounts
+        </p>
         <h2 className="mt-[24px] font-semibold sm:text-[32px] text-[24px] text-white">
           {title}
         </h2>
       </div>
     )}
   </motion.div>
-
 );
 
 export default ExploreCard;
